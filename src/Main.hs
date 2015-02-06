5ab2e2d345d946145cf9e99291cf114772ae5c30
Yves Dubromelle <taeradan@dubronetwork.fr>
2015-02-06 13:39:07 +0100

    only print the files with errors

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,50 @@
 module Main where
 
+import           Monitoring.Shinken.Configuration
 import           Monitoring.Shinken.Parser
 
 import           Data.Either
+import           Data.List                        hiding (find)
 import           System.Directory
 import           System.Environment
 import           System.FilePath.Find
 import           System.IO
+import           Text.Parsec
 
 pattern = "*.cfg"
 
 main = do
         dir   <- getCurrentDirectory
         files <- search dir
-        putStrLn "* Found Files:"
-        mapM_ putStrLn files
-        putStrLn ""
         content <- mapM readFile files
         let normalisedContent = map normaliseComments content
-            objects = concat . rights $ map parseConfigFile (zip files normalisedContent)
-        putStrLn "* Parsed objects:"
-        print $ objects
-        putStrLn ""
+            objects = map parseConfigFile (zip files normalisedContent)
+        --putStrLn "* Found Files:"
+        --mapM_ putStrLn files
+        --putStrLn ""
+        --putStrLn "* Parsed objects:"
+        --mapM_ print objects
+        --putStrLn ""
+        let all = zip files objects
+            errors = filter (\ (_,x) -> isLeft x) all
+            goods = filter (\ (_,x) -> isRight x) all
+        mapM_ printBoth $ errors
         putStrLn "* Stats"
         putStr "    * Files found: "
         print $ length files
         putStr "    * Objects found: "
-        print $ length objects
+        print . length . concat . rights $ objects
+        putStr "    * Files with errors: "
+        print $ length errors
 
 search = find always (fileName ~~? pattern &&? fileName /~? "shinken.cfg")
 
 normaliseComments :: String -> String
 normaliseComments = map (\ x -> if x == ';' then '#' else x)
+
+printBoth :: (FilePath, Either ParseError [Object]) -> IO ()
+printBoth (filepath, object) = do
+    putStr "***** File: "
+    putStrLn filepath
+    print object
+    putStrLn ""
